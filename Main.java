b431c78a1ede46205564cfa3e4bdc7b327509373
Maximilian <dolberg.maximilian@guj.de>
2023-05-31 09:29:04 +0200

    Implementierung von While-Loop

--- Main.java
+++ Main.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Anlegen wichtiger Variablen
+        Boolean running = true;
 
         System.out.println(Antworten.HALLO.getAntwort());
-        Scanner reader = new Scanner(System.in);  //Erstellung eines neuen Scanner-Objektes
+        Scanner reader = new Scanner(System.in);  // Erstellung eines neuen Scanner-Objektes
         System.out.println("Gib eine Frage ein: ");
-        String input = reader.nextLine(); // Scannt den nächsten String
-        reader.close();
 
-        if (input.contains("Hilfe")) { // Suche in der Frage nach diesem Wort
-            System.out.println("Ich versuche zu helfen.");
-        } else {
-            System.out.println(Antworten.WEITERLEITUNG.getAntwort());
+        while (running) {
+
+            String input = reader.nextLine(); // Scannt den nächsten String
+
+            if (input.contains("Hilfe")) { // Suche in der Frage nach diesem Wort
+                System.out.println("Ich versuche zu helfen.");
+            } else if (input.contains("ende")) {
+                reader.close();
+                break;
+            } else {
+                System.out.println(Antworten.WEITERLEITUNG.getAntwort());
+            }
         }
     }
 }
