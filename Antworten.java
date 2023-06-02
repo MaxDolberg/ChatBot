f433adffb50bbd2974fc01ccc162eef65a86217e
Maximilian <dolberg.maximilian@guj.de>
2023-06-02 09:37:05 +0200

    Formatierung

--- Antworten.java
+++ Antworten.java
@@ -2,8 +2,8 @@ public enum Antworten {
     HALLO("Wilkommen! Ich bin der ChatBot der Solution GmbH. Wie kann ich Ihnen behilflich sein?"),
     WLAN("Wenn Sie Probleme mit dem WLAN haben, versuchen Sie den Router für 10 Minuten vom Strom zu nehmen."),
     HELP("Es freut mich, dass ich Ihnen helfen konnte. Ich wünsche einen guten Tag."),
-    NO_HELP("Es tut mir leid, dass ich Ihnen nicht weiterhelfen konnte. Bitte kontaktieren Sie den Kundensupport unter folgender Nummer: 012 34567"),
-    WEITERLEITUNG("Bei dieser Frage kann ich leider nicht weiterhelfen. Bitte kontaktieren Sie den Kundensupport unter folgender Nummer: 012 34567");
+    NO_HELP("Es tut mir leid, dass ich Ihnen nicht weiterhelfen konnte. Bitte kontaktieren Sie den Kundensupport unter folgender Nummer: 012 3456789"),
+    WEITERLEITUNG("Bei dieser Frage kann ich leider nicht weiterhelfen. Bitte kontaktieren Sie den Kundensupport unter folgender Nummer: 012 3456789");
 
     private String antwort;
  
--- Main.java
+++ Main.java
@@ -17,16 +17,21 @@ public class Main {
             if (message.contains("hilfe")) { // Suche in der Frage nach diesem Wort
                 System.out.println("Ich versuche zu helfen.");
                 System.out.println("Hat Ihnen diese Antwort geholfen? \n 1 - Ja \n 2 - Nein");
+
             } else if (message.contains("wlan")) {
                 System.out.println(Antworten.WLAN.getAntwort());
                 System.out.println("Hat Ihnen diese Antwort geholfen? \n 1 - Ja \n 2 - Nein");
+
             } else if (message.contains("1")) {
                 System.out.println(Antworten.HELP.getAntwort());
+
             } else if (message.contains("2")) {
                 System.out.println(Antworten.NO_HELP.getAntwort());
+
             } else if (message.contains("ende")) {
                 reader.close();
                 break;
+                
             } else {
                 System.out.println(Antworten.WEITERLEITUNG.getAntwort());
             }
