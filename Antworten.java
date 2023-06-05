4f063c1ea5229bc591de3d39830451cbddc09510
githubuser0912 <118199740+githubuser0912@users.noreply.github.com>
2023-06-05 10:13:39 +0200

    Test

--- Antworten.java
+++ Antworten.java
@@ -3,8 +3,9 @@ public enum Antworten {
     WLAN("Wenn Sie Probleme mit dem WLAN haben, versuchen Sie den Router für 10 Minuten vom Strom zu nehmen."),
     HELP("Es freut mich, dass ich Ihnen helfen konnte. Ich wünsche einen guten Tag."),
     NO_HELP("Es tut mir leid, dass ich Ihnen nicht weiterhelfen konnte. Bitte kontaktieren Sie den Kundensupport unter folgender Nummer: 012 3456789"),
-    WEITERLEITUNG("Bei dieser Frage kann ich leider nicht weiterhelfen. Bitte kontaktieren Sie den Kundensupport unter folgender Nummer: 012 3456789");
-
+    WEITERLEITUNG("Bei dieser Frage kann ich leider nicht weiterhelfen. Bitte kontaktieren Sie den Kundensupport unter folgender Nummer: 012 3456789"),
+    TEST("Test");
+    
     private String antwort;
  
     Antworten(String reply) {
