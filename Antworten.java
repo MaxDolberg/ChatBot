f21566fa63f146c68d2537da30242b3786de5538
Meyer, Maximilian <meyer.maximilian@guj.de>
2023-06-07 14:21:30 +0200

    Tastatur 2 Hinzugefügt

--- Antworten.java
+++ Antworten.java
@@ -10,8 +10,10 @@ public enum Antworten {
     BILDSCHIRM("Wenn Sie ein Problem mit Ihrem Bildschirm haben, sollten Sie am besten Ihren Monitor vom PC trennen und erneut anschließen."),
     BILDSCHIRM2("Da die erste Lösung nicht geholfen hat, ziehen Sie den Stromstecker vom Monitor raus und warten Sie 10 Sekunden. Danach stecken Sie ihn bitte wieder ein und probieren es erneut.");
     TASTATUR("Wenn Sie ein Problem mit Ihrer Tastatur haben, entfernen Sie die Tastatur einmal von Ihrem PC für einige Zeit und schließen Sie die Tastatur erneut an"),
+    TASTATUR2("Falls das nicht geholfen hat überprüfen sie ob Ihre Tastatur eine Baterrie benötigt und ob sie eingeschaltet ist."),
     MAUS("Wenn Sie ein Problem mit Ihrer Maus haben, entfernen Sie die Maus einmal von Ihrem PC für einige Zeit und schließen Sie die Maus erneut an"),
     
+
     private String antwort;
  
     Antworten(String reply) {
